d55d33d5801e23fcddf0b2fedbcc48091c0f38c4
Mikel Zhobro <zhobromikel@gmail.com>
2021-08-19 10:15:29 +0200

    correct opt  prob

--- OptimizationDesiredInput.m
+++ OptimizationDesiredInput.m
@@ -9,15 +9,11 @@ classdef OptimizationDesiredInput < matlab.System
             obj.lss = lifted_state_space;
         end
 
-        function u_des = calcDesiredInput(obj, dup, y_des) % give as input Tb, or N_Tb(from which index Tb starts)
-            %CALCDESIREDINPUT Once we have the desired motion y_des for the
-            %plate and have estimated disturbances we can compute the
-            %"optimal" feedforward v_des by solving the QP problem.
-            %TODO: maybe constrained QP (to make sure state doesnt gocrazy)
-            %   dup: [ndup*(N+1),1]  [dup_0,..dip_(N-1)]
-            %   ydes: [ndy*(N+1),1]   [ydes_0,..ydes_N]
-            u_des = linsolve((transpose(obj.lss.GF) * obj.lss.GF), transpose(obj.lss.GF)*(obj.lss.GK*dup + obj.lss.Gd0 - y_des));
-            % u_des = -obj.GFTGF_1 * (obj.GK*dup + obj.Gd0 - y_des)*obj.GF;
+        function u_des = calcDesiredInput(obj, dup, y_des)
+%             u_des = linsolve(GF, -transpose(GF)*(GK*dup + Gd0 - y_des));
+            u_des = quadprog((transpose(obj.lss.GF) * obj.lss.GF), transpose(obj.lss.GF)*(obj.lss.GK*dup + obj.lss.Gd0 - y_des));
+            %% check
+%             norm( (transpose(GF) * GF)* u_des + transpose(GF)*(GK*dup + Gd0 - y_des) )
         end
     end
 end
