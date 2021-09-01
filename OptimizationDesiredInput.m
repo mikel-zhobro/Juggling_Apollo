46f50c0e138f006c84866c75c1aed23b982c44ca
jotix16 <zhobromikel@gmail.com>
2021-09-01 20:02:37 +0200

    todo on optimization

--- OptimizationDesiredInput.m
+++ OptimizationDesiredInput.m
@@ -11,6 +11,7 @@ classdef OptimizationDesiredInput < matlab.System
 
         function u_des = calcDesiredInput(obj, dup, y_des)
 %             u_des = linsolve(GF, -transpose(GF)*(GK*dup + Gd0 - y_des));
+            % Add penalty on the input
             u_des = quadprog((transpose(obj.lss.GF) * obj.lss.GF), transpose(obj.lss.GF)*(obj.lss.GK*dup + obj.lss.Gd0 - y_des));
             %% check
 %             norm( (transpose(obj.lss.GF) * obj.lss.GF)* u_des + transpose(obj.lss.GF)*(obj.lss.GK*dup + obj.lss.Gd0 - y_des) )
