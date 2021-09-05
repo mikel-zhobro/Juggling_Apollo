8ea33c6e4ded09885f5e2365c08f46ea8dc656fc
Mikel Zhobro <zhobromikel@gmail.com>
2021-09-05 23:50:24 +0200

    finish simulation and tests

---
+++ py_pro/juggling_apollo/Simulation.py
@@ -0,0 +1,146 @@
+
+import numpy as np
+from utils import steps_from_time, find_continuous_intervals, plot_intervals, plt
+from settings import m_b, m_p, k_c, g
+
+
+class Simulation:
+    # Constructor
+    def __init__(self, input_is_force, air_drag, sys=None, plate_friction=False):
+        # true if input is force, false if input is velocity
+        self.input_is_force = input_is_force
+        # dynamic sys used to get state space matrixes of system(TODO)
+        self.sys = sys
+        # bool whether we should add air drag to the ball
+        self.air_drag = air_drag
+        # bool whether we should add some friction to the plate movement
+        self.plate_friction = plate_friction
+
+    def force_from_velocity(self, u_des_p, u_p):
+        F_p = m_p * k_c * (u_des_p - u_p)
+        return F_p
+
+    def simulate_one_iteration(self, dt, T, x_b0, x_p0, u_b0, u_p0, u, repetitions=1, d=None):
+        """ Simulates the system from the time interval 0->T
+
+        Args:
+            dt ([double]): [description]
+            T ([double]): [description]
+            x_b0 ([double]): [description]
+            x_p0 ([double]): [description]
+            u_b0 ([double]): [description]
+            u_p0 ([double]): [description]
+            u ([np.array(double)]): [description]
+            d ([np.array(double)], optional): [description]. Defaults to None.
+            repetitions (int, optional): [description]. Defaults to 1.
+
+        Returns:
+            [type]: x_b, u_b, x_p, u_p, dP_N_vec, gN_vec, u_vec of shape [1, N]
+        """
+        if d is None:
+            d = np.zeros(u.shape)  # disturbance
+
+        u = np.repeat(u, repetitions)
+        d = np.repeat(d, repetitions)
+        # Vectors to collect the history of the system states
+        N = steps_from_time(T, dt) * repetitions
+        x_b = np.zeros((N, 1)); x_b[0] = x_b0
+        u_b = np.zeros((N, 1)); u_b[0] = u_b0
+        x_p = np.zeros((N, 1)); x_p[0] = x_p0
+        u_p = np.zeros((N, 1)); u_p[0] = u_p0
+        # Vector to collect extra info for debugging
+        dP_N_vec = np.zeros((N, 1))
+        gN_vec = np.zeros((N, 1))
+        u_vec = np.zeros((N, 1))
+
+        # Simulation
+        for i in range(N-1):
+            # one step simulation
+            x_b_new, x_p_new, u_b_new, u_p_new, dP_N, gN, u_i = self.simulate_one_step(dt, u[i], x_b[i], x_p[i], u_b[i], u_p[i], d[i])
+            # collect state of the system
+            x_b[i+1] = x_b_new
+            x_p[i+1] = x_p_new
+            u_b[i+1] = u_b_new
+            u_p[i+1] = u_p_new
+            # collect helpers
+            dP_N_vec[i] = dP_N
+            gN_vec[i] = gN
+            u_vec[i] = u_i
+        return x_b, u_b, x_p, u_p, dP_N_vec, gN_vec, u_vec
+
+    def simulate_one_step(self, dt, u_i, x_b_i, x_p_i, u_b_i, u_p_i, plate_force_disturbance):
+        # this works only with force as input
+        # so if we get speed we transform it to force.
+        F_i = u_i
+        if not self.input_is_force:  # did we get speed?
+            F_i = self.force_from_velocity(u_i, u_p_i)
+
+        # disturbances
+        ball_friction_force_disturbance = self.get_ball_force_friction(u_b_i)
+        gravity_force = dt*(g + ball_friction_force_disturbance/m_b)
+        F_i = F_i + plate_force_disturbance
+
+        x_b_1_2 = x_b_i + 0.5*dt*u_b_i
+        x_p_1_2 = x_p_i + 0.5*dt*u_p_i
+
+        # gN = x_b_1_2 - x_p_1_2
+        gN = x_b_i - x_p_i
+        gamma_n_i = u_b_i - u_p_i
+        # && (((-gamma_n_i + g*dt + u_i*dt/m_p))>=0)
+        contact_impact = gN <= 1e-5
+        if contact_impact:
+            dP_N = max(0, (-gamma_n_i + gravity_force + F_i*dt/m_p) / (m_b ** -1 + m_p ** -1))
+            # dP_N = (-gamma_n_i + g*dt + u_i*dt/m_p)/ (m_b**-1 + m_p**-1)
+        else:
+            dP_N = 0
+
+        u_b_new = u_b_i - gravity_force + dP_N / m_b
+        u_p_new = u_p_i + F_i * dt / m_p - dP_N / m_p
+        x_b_new = x_b_1_2 + 0.5 * dt * u_b_new
+        x_p_new = x_p_1_2 + 0.5 * dt * u_p_new
+        return x_b_new, x_p_new, u_b_new, u_p_new, dP_N, gN, F_i
+
+    def get_ball_force_friction(self, v):
+        f_drag = 0
+        if self.air_drag:
+            # D is the diameter of the ball
+            D = 0.4  # ball has diameter of 5cm
+            p = 1.225  # [kg/m]  air density
+            c = np.pi/16*p* (D**2)  # c = 1/4*p*A = pi/16*p*D**2
+            f_drag = np.sign(v)*c* (v**2)
+        return f_drag
+
+
+def plot_simulation(dt, u, x_b, u_b, x_p, u_p, dP_N_vec, gN_vec):
+    # Everything are column vectors
+    intervals = find_continuous_intervals(1 + np.argwhere(np.squeeze(gN_vec) <= 1e-5))
+    fig, axs = plt.subplots(5, 1)
+
+    timesteps = np.arange(u_p.size) * dt
+    axs[0].plot(timesteps, x_b, 'r', label='Ball position [m]')
+    axs[0].plot(timesteps, x_p, 'b', label='Plate position [m]')
+    axs[1].plot(timesteps, u_b, 'r', label='Ball velocity [m/s]')
+    axs[1].plot(timesteps, u_p, 'b', label='Plate velocity [m/s]')
+    axs[2].plot(timesteps, dP_N_vec, 'b', label='dP_N')
+    axs[3].plot(timesteps, gN_vec, 'b', label='g_{N_{vec}} [m]')
+    axs[4].plot(timesteps, u, 'b', label='F [N]')
+    for ax in axs:
+        ax = plot_intervals(ax, intervals, dt)
+        # Position where legend can be put
+        # ===============   =============
+        # Location String   Location Code
+        # ===============   =============
+        # 'best'            0
+        # 'upper right'     1
+        # 'upper left'      2
+        # 'lower left'      3
+        # 'lower right'     4
+        # 'right'           5
+        # 'center left'     6
+        # 'center right'    7
+        # 'lower center'    8
+        # 'upper center'    9
+        # 'center'          10
+        # ===============   =============
+        ax.legend(loc=1)
+    plt.show()
---
+++ py_pro/tests/test_Simulation.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python2
+import path_setter
+import numpy as np
+from juggling_apollo.utils import plan_ball_trajectory, steps_from_time
+from juggling_apollo.Simulation import Simulation, plot_simulation
+from juggling_apollo.settings import dt, m_p
+
+
+def test1():
+  input_is_force = False
+  air_drag = True
+
+  # Design params
+  h_b_max = 1                                        # [m] maximal height the ball achievs
+  Tb, _ = plan_ball_trajectory(h_b_max, 0, 0)        # [s] flying time of the ball
+  Tsim= Tb*2*5                                       # [s] simulation time
+  N = steps_from_time(Tsim, dt)           # number of steps for one iteration (maybe use floor)
+
+  # Initial Conditions
+  x_b0 = 0       # [m]   starting ball position
+  x_p0 = 0       # [m]   starting plate position
+  u_p0 = 0       # [m/s] starting plate velocity
+  u_b0 = u_p0    # [m/s] starting ball velocity
+
+  # %% Simulation Example
+  # Input
+  A = 0.3                                            # [m] amplitude
+  timesteps = dt * np.arange(N)                             # [s,s,..] timesteps
+  F_p = 100 * m_p * A*np.sin(np.pi/Tb *timesteps)          # [N] input force on the plate
+  dist = 0.1 * np.sin(timesteps)
+  input_is_force = True
+
+  # Simulation for N steps
+  sim = Simulation(input_is_force, air_drag)
+  [x_b, u_b, x_p, u_p, dP_N_vec, gN_vec, F_vec] = sim.simulate_one_iteration(dt, Tsim, x_b0, x_p0, u_b0, u_p0, F_p, repetitions=1, d=dist)
+
+  # Plotting for Simulation Example
+  plot_simulation(dt, F_vec, x_b, u_b, x_p, u_p, dP_N_vec, gN_vec)
+
+
+def test_all():
+  test1()
+
+
+if __name__ == "__main__":
+  test_all()
