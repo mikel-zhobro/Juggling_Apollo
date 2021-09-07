f263b7d6f28b512555e13d899148e7104935a4aa
jotix16 <zhobromikel@gmail.com>
2021-09-07 20:06:31 +0200

    py add regularization in optim

--- py_pro/juggling_apollo/OptimLss.py
+++ py_pro/juggling_apollo/OptimLss.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 import numpy as np
 
 
@@ -7,9 +6,11 @@ class OptimLss:
     self.lss = lifted_state_space
 
   def calcDesiredInput(self, dup, y_des, print_norm=False):
+    # Solving the problem u_des = argmin_u||GFu + GKdu_p + Gd0 - ydes||_2 +0.000001|u|_2
+    # Tu = b
     # TODO: Add penalty on the input
-                       # transpose(obj.lss.GF) * obj.lss.GF, -transpose(obj.lss.GF)*(obj.lss.GK*dup + obj.lss.Gd0 - y_des));
-    u_des = np.linalg.lstsq((self.lss.GF.T).dot(self.lss.GF), -self.lss.GF.T.dot(self.lss.GK.dot(dup) + self.lss.Gd0 - y_des), rcond=None)[0]
+    u_des = np.linalg.lstsq((self.lss.GF.T).dot(self.lss.GF)+ 0.00000001*np.eye(self.lss.GF.shape[1]), -self.lss.GF.T.dot(self.lss.GK.dot(dup) + self.lss.Gd0 - y_des), rcond=None)[0]
     if print_norm:
-      print("The norm of the optimization error is:", np.linalg.norm((self.lss.GF.T).dot(self.lss.GF).dot(u_des) + self.lss.GF.T.dot((self.lss.GK.dot(dup) + self.lss.Gd0 - y_des))))
+      y = self.lss.GF.dot(u_des) + self.lss.Gd0  # predicted y
+      print("The norm of the optimization error is:", np.linalg.norm(y - y_des))
     return u_des
