87d9f4a1b4e14125bf3efcdf59bd45d889340e93
jotix16 <zhobromikel@gmail.com>
2021-09-08 15:05:22 +0200

    remove todo

--- py_pro/juggling_apollo/OptimLss.py
+++ py_pro/juggling_apollo/OptimLss.py
@@ -7,8 +7,6 @@ class OptimLss:
 
   def calcDesiredInput(self, dup, y_des, print_norm=False):
     # Solving the problem u_des = argmin_u||GFu + GKdu_p + Gd0 - ydes||_2 +0.000001|u|_2
-    # Tu = b
-    # TODO: Add penalty on the input
     u_des = np.linalg.lstsq((self.lss.GF.T).dot(self.lss.GF)+ 0.00000001*np.eye(self.lss.GF.shape[1]), -self.lss.GF.T.dot(self.lss.GK.dot(dup) + self.lss.Gd0 - y_des), rcond=None)[0]
     if print_norm:
       y = self.lss.GF.dot(u_des) + self.lss.Gd0  # predicted y
