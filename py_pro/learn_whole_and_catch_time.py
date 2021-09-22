eb985d37f5d48c5d1b8f42b3ce993f77ff96c9f1
jotix16 <zhobromikel@gmail.com>
2021-09-22 11:46:54 +0200

    1 ball update

--- py_pro/learn_whole_and_catch_time.py
+++ py_pro/learn_whole_and_catch_time.py
@@ -1,8 +1,8 @@
 # %%
 import numpy as np
+from juggling_apollo.settings import dt, g, ABS
 from juggling_apollo.utils import plan_ball_trajectory, steps_from_time, plotIterations, plt
 from juggling_apollo.Simulation import Simulation, plot_simulation
-from juggling_apollo.settings import dt
 from juggling_apollo.ILC import ILC
 from learnThrow import learnThrow
 
@@ -21,7 +21,14 @@ x_ruhe = -0.4
 x0 = [x_ruhe, x_ruhe, 0, 0]  # the plate and ball in ruhe
 
 # %%
+# Learn Throw
+ILC_it = 13  # number of ILC iteration
+ub_0 = ub_00
+t_catch = 2*Th/3
+N_catch_1=steps_from_time(t_catch, dt)-1
+t_throw = Th - t_catch
 
+# Init ilc
 kf_dpn_params = {
   'M': 0.031*np.eye(N_1, dtype='float'),      # covariance of noise on the measurment
   'P0': 0.1*np.eye(N_1, dtype='float'),     # initial disturbance covariance
@@ -29,21 +36,13 @@ kf_dpn_params = {
   'epsilon0': 0.3,                          # initial variance of noise on the disturbance
   'epsilon_decrease_rate': 1              # the decreasing factor of noise on the disturbance
 }
+my_ilc = ILC(dt, kf_dpn_params=kf_dpn_params, x_0=x0)
+my_ilc.initILC(N_1=N_1, impact_timesteps=[False]*N_1)  # ignore the ball
 
-my_ilc = ILC(dt, kf_dpn_params=kf_dpn_params, x_0=x0, t_f=Tb, t_h=Th)
 
 sim = Simulation(input_is_force=False, x0=x0, air_drag=True, plate_friction=True)
 sim.reset()
-# Learn Throw
-ILC_it = 13  # number of ILC iteration
-ub_0 = ub_00
-t_catch = 2*Th/3
-N_catch_1=steps_from_time(t_catch, dt)-1
-t_throw = Th - t_catch
-# reset ilc
 
-my_ilc.initILC(N_1=N_1, impact_timesteps=[False]*N_1)  # ignore the ball
-y_des, u_ff, ub_0, t_catch = my_ilc.learnWhole(ub_00, t_catch=t_catch)
 
 # collect: dup, x_p, x_b, u_p
 dup_vec = np.zeros([ILC_it, my_ilc.kf_dpn.d.size], dtype='float')
@@ -55,17 +54,28 @@ u_d2_vec = np.zeros([ILC_it, 1], dtype='float')
 u_Tb_vec = np.zeros([ILC_it, 1], dtype='float')
 
 # ILC Loop
+u_ff =None
 d1_meas = 0
 d2_meas = 0
 y_meas = None
 
 # disturbance to be learned
 period = 0.02/dt
-# TODO: (disturbance can be a generator function)
 disturbance = 150*np.sin(2*np.pi/period*np.arange(my_ilc.N_1), dtype='float')  # disturbance on the plate position(0:my_ilc.N_1-1)
 
+# Min Jerk Params
+# new MinJerk
+t_end = Tb + Th
+# a0 = None;        a1 = None;       a2 = None;          a3 = None
+tt=[0,        t_throw,    t_throw+Tb/4,    t_end-t_catch,    t_end]
+xx=[x0[0],    0.0,       -0.2,             0.0,              x0[0]]
+uu=[x0[2],    ub_0,       0.0,            -ub_0/3,           x0[2]]
 
 for j in range(ILC_it):
+  # Learn feed-forward signal
+  uu[1] = ub_0
+  y_des, u_ff = my_ilc.learnWhole(tt=tt, xx=xx, uu=uu, u_ff_old=u_ff, y_meas=y_meas)
+
   # Main Simulation
   x000 = [x0[0], x0[1], x0[2], x0[3]]
   [x_b, u_b, x_p, u_p, dP_N_vec, gN_vec, F_vec] = \
@@ -75,24 +85,21 @@ for j in range(ILC_it):
   #    plot_simulation(dt, F_vec, x_b, u_b, x_p, u_p, dP_N_vec, gN_vec, y_des, title="Iteration: " + str(j),
   #                    vertical_lines={Th-t_catch: "T_throw", Tb+Th-t_catch: "T_catch"})
 
-  # Throw point
-  N_throw_time = np.argmax(gN_vec[:N_half_1]>1e-5)
-  T_throw_time = N_throw_time*dt
   # Measurments
-  N_catch_time = N_half_1+np.argmax(gN_vec[N_half_1:]<=1e-5)
-  t_throw = Th - t_catch
-  fly_time_meas = N_catch_time*dt - t_throw
+  # a. System output
   y_meas = x_p[1:]
-  d1_meas = max(x_b) - Hb  # disturbance on height
+  # b. Throw point
+  N_throw_time = np.argmax(gN_vec[:N_half_1]>ABS)
+  T_throw_time = N_throw_time*dt
+  # c. Catch point
+  N_catch_time = N_half_1+np.argmax(gN_vec[N_half_1:]<=ABS)
+  fly_time_meas = N_catch_time*dt - t_throw
   d2_meas = fly_time_meas - Tb  # disturbance on ball flight time
-  # d3_meas = 0
-  # d3_meas = d2_meas
-  N_catch_1=steps_from_time(float(t_catch), dt)-1
-  d3_meas = - max(gN_vec[-N_catch_1:])
-  # d3_meas =max(x_b[-N_h2_1:] - x0[0])
+  # d. Ball Height
+  d1_meas = max(x_b) - Hb  # disturbance on height
 
-  # LEARN THROW
-  y_des, u_ff, ub_0, t_catch = my_ilc.learnWhole(ub_0=ub_0, t_catch=t_catch, u_ff_old=u_ff, y_meas=y_meas, d1_meas=d1_meas, d2_meas=d2_meas, d3_meas=0)
+  # Newton updates
+  ub_0 = ub_0 - 0.3*0.5*g*d2_meas  # move in oposite direction of error # TODO: vectorized
 
   # 5. Collect data for plotting
   # dup_vec[j, :] = np.squeeze(my_ilc.kf_dpn.d)
@@ -110,10 +117,10 @@ for j in range(ILC_it):
         + ", \n\tError on throw hight: " + str(x_p[N_throw_time])
         + ", \n\tError on catch time: " + str(d2_meas)
         + ", \n\tError on catch hight: " + str(x_p[N_catch_time])
-        + ", \n\tThrow/Catch time: " + str(t_throw) + " / " + str(t_catch)  
-        + ", \n\tBiggest jump after catch: " + str(-d3_meas)
+        + ", \n\tThrow/Catch time: " + str(t_throw) + " / " + str(t_catch)
         ) 
 
+# Evauluate last iteration
 plot_simulation(dt, F_vec, x_b, u_b, x_p, u_p, dP_N_vec, gN_vec, y_des, title="Iteration: " + str(ILC_it),
                 vertical_lines={Th-t_catch: "T_throw", Tb+Th-t_catch: "T_catch"})
 
