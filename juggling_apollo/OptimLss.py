64b1bf6a0c03826eb0a751ee28f251dd061b2203
Apollo <apollo@adrian>
2021-11-01 15:47:30 +0100

    optim simplifz

--- juggling_apollo/OptimLss.py
+++ juggling_apollo/OptimLss.py
@@ -5,11 +5,12 @@ class OptimLss:
   def __init__(self, lifted_state_space):
     self.lss = lifted_state_space
 
-  def calcDesiredInput(self, dup, y_des, print_norm=False):
-    # Solving the problem u_des = argmin_u||GFu + GKdu_p + Gd0 - ydes||_2 +0.00000001|u|_2
-    u_des = np.linalg.lstsq((self.lss.GF.T).dot(self.lss.GF) + 0.00000001*np.eye(self.lss.GF.shape[1]), 
-                            -self.lss.GF.T.dot(self.lss.GK.dot(dup) + self.lss.Gd0 - y_des), rcond=None)[0]
-    
+  def calcDesiredInput(self, d, y_des, print_norm=False):
+    # Solves  GFu = ydes -(GKd + Gd0)
+    # as optimization u_des = argmin_u||[ydes - (GKd + Gd0)] - GFu||_2 +0.00000001|u|_2
+    # u_des = np.linalg.lstsq((self.lss.GF.T).dot(self.lss.GF) + 0.0000000*np.eye(self.lss.GF.shape[1]), 
+    #                          self.lss.GF.T.dot(y_des - self.lss.GK.dot(d) - self.lss.Gd0), rcond=None)[0]
+    u_des = np.linalg.lstsq(self.lss.GF, y_des - self.lss.GK.dot(d) - self.lss.Gd0, rcond=None)[0]
     
     if print_norm:
       y = self.lss.GF.dot(u_des) + self.lss.Gd0  # predicted y
