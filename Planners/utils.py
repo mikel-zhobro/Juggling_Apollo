edfb2cc0ad2a930f7694a27972a5f09fb0632f6a
Mikel Zhobro <zhobromikel@gmail.com>
2021-12-10 14:43:59 +0100

    Planners: use  joint planner

--- Planners/utils.py
+++ Planners/utils.py
@@ -4,5 +4,5 @@ PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir
 sys.path.append(PROJECT_ROOT)
 
 from juggling_apollo.utils import *
-
+from examples.utils import plot_A
 from juggling_apollo.settings import g
