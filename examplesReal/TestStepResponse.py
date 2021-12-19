6a970bc7d79c2cfeedfc5c06d3f7fa853cbad08d
jotix16 <zhobromikel@gmail.com>
2021-12-19 13:50:40 +0100

    add tests

--- examplesReal/TestStepResponse.py
+++ examplesReal/TestStepResponse.py
@@ -1,5 +1,6 @@
 import numpy as np
 import time
+import os
 
 import __add_path__
 from juggling_apollo.settings import dt
@@ -31,38 +32,38 @@ N_1 = 1000; T_FULL=N_1*dt
 N_step = 150; N_start = (N_1-N_step)//2
 
 
-step_value = -1.0
-try_out_joints = [0,1,2,3,4,5,6, 7]
-for i in try_out_joints:
-  u_ff = np.zeros((N_1,7,1))
-  if i==7:
-    u_ff[N_start:N_start+N_step,:,0] = -step_value
-    u_ff[N_start:N_start+N_step,1,0] *= -1.
-  else:
-    u_ff[N_start:N_start+N_step,i,0] = -step_value if i !=1 else step_value
 
-  ####################################################################################################################################
-  q_traj, q_v_traj, q_a_traj, F_N_vec, u_vec = rArmInterface.apollo_run_one_iteration(dt=dt, T=T_FULL, u=u_ff, joint_home_config=q_start, repetitions=1)
+step_value = -0.6
+try_out_joints = [
+  [[0],[1],[2],[3],[4],[5],[6]],
+  [list(range(7))],
+  ]
+
+for step_value in [-0.1, -0.2, -0.3, -0.4, -0.5, -0.6, -0.7, -0.8, -0.9, -1.0]:
+  q_v_traj_vec = np.zeros((1, N_1, N_joints, 1))
+  uff_collect = np.zeros((1, N_1, N_joints, 1))
+
+  for learnable_joints_l in try_out_joints:
+    for learnable_joints in learnable_joints_l:
+
+      # Prepare input
+      u_ff = np.zeros((N_1,7,1))
+      for i in learnable_joints:
+        u_ff[N_start:N_start+N_step,i,0] = -step_value if i !=1 else step_value
 
 
-  learnable_joints = [i] if i<7 else list(range(7))
-  plot_info(dt, learnable_joints=learnable_joints,
-            u_ff_vec=u_ff[np.newaxis, :], q_v_traj=q_v_traj[1:],
-            v=True, p=False, dp=False, e_xyz=False, e=False, torque=False, N=1,
-            fname="/home/apollo/Desktop/Investigation/v_{}_joint_{}_{}".format(str(abs(step_value)).replace('.', ''), learnable_joints, time.strftime("%Y_%m_%d-%H_%M_%S")))
+      q_traj, q_v_traj, q_a_traj, F_N_vec, u_vec = rArmInterface.apollo_run_one_iteration(dt=dt, T=T_FULL, u=u_ff, joint_home_config=q_start, repetitions=1)
 
+      for i in learnable_joints:
+        uff_collect[0,:,i] = (180.0 / np.pi) * u_ff[:,i]
+        q_v_traj_vec[0,:,i] = (180.0 / np.pi) * q_v_traj[1:,i]
 
-  SAVING = True
-  if SAVING:
-    # Saving Results
-    filename = "examplesReal/dataReal/StepResponseTest/step_val_{}_joints_{}_".format(step_value, i if i<7 else 'all') + time.strftime("%Y_%m_%d-%H_%M_%S.txt")
-    save(filename,
-         u_ff=u_ff, learnable_joints = learnable_joints,
-         T_home=T_home,                                                                    # Home
-         q_v_traj=q_v_traj,                                                      # Joint Informations
-         dt=dt
-        )  # ILC parameters
+    dir_name = "/home/apollo/Desktop/Investigation/Step_Response/{}".format(time.strftime("%Y_%m_%d"))
+    if not os.path.exists(dir_name):
+      os.makedirs(dir_name)
+    plot_info(dt, learnable_joints=[l for ll in learnable_joints_l for l in ll],
+              u_ff_vec=uff_collect, q_v_traj=q_v_traj_vec[0],
+              v=True, p=False, dp=False, e_xyz=False, e=False, torque=False, N=1,
+              fname=dir_name + "/v_{}_joint_{}".format(str((180.0 / np.pi) * abs(step_value)).replace('.', ''), learnable_joints_l))
 
-    with open('examplesReal/dataReal/StepResponseTest/list_files.txt', 'a') as f:
-      f.write(filename + "\n")
 
