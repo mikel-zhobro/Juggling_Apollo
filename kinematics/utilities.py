e1a9ddc3d2e7a53c23b3ccb8a2c3c4ab7013b3bb
Mikel Zhobro <zhobromikel@gmail.com>
2021-12-29 17:26:42 +0100

    ApolliIT: add velocity limits

--- kinematics/utilities.py
+++ kinematics/utilities.py
@@ -2,7 +2,7 @@ import os, sys
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
 sys.path.append(PROJECT_ROOT)
 
-from apollo_interface.Apollo_It import JOINTS_LIMITS, R_joints, L_joints
+from apollo_interface.Apollo_It import JOINTS_LIMITS, R_joints, L_joints, JOINTS_V_LIMITS
 import numpy as np
 from scipy.linalg import logm
 import math
