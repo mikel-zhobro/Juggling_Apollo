b47324ac496b9532c3c2e242db08dc29f0f7b6a6
Apollo <apollo@adrian>
2022-01-10 08:33:25 +0100

    utils: better plotting

--- examples/utils.py
+++ examples/utils.py
@@ -5,8 +5,7 @@ import time
 import numpy as np
 import pickle
 
-from juggling_apollo.utils import DotDict
-
+from juggling_apollo.utils import DotDict, full_extent
 
 colors = ["r", 'g', 'b', 'k', 'c', 'm', 'y']
 line_types = ["-", "--", ":", '-.']
@@ -19,7 +18,7 @@ def plot_A(lines_list, indexes_list=list(range(7)), labels=None, dt=1, xlabel=""
   M = len(indexes_list)
   lines_list = A*np.asarray(lines_list)
   timesteps = dt*np.arange(lines_list[0].shape[0])
-  fig, axs = plt.subplots(rows,M/rows, figsize=(18, 3), sharex=True, sharey=True)
+  fig, axs = plt.subplots(M/rows, rows, figsize=(6, 18), sharex=True, sharey=True)
 
   axs = np.array(axs)
   lines2 = []
@@ -27,7 +26,8 @@ def plot_A(lines_list, indexes_list=list(range(7)), labels=None, dt=1, xlabel=""
     ix = indexes_list[iii]
     for i in range(N):
       lines2 += ax.plot(timesteps,  lines_list[i][:, ix].squeeze(),
-                              linestyle=line_types[i%len(line_types)],linewidth=1,
+                              linestyle="-" if i ==0 else '--',
+                              linewidth=1, marker="x"if i ==0 else None, markersize = 2,
                               label= i*"_" + (r"$\theta_{}$".format(ix+1) if index_labels is None else index_labels[ix]))
     if limits is not None:
       ax.axhspan(A*limits[iii].a, A*limits[iii].b, color='gray', alpha=0.2)  # color=l[0].get_color()
@@ -55,12 +55,12 @@ def plot_A(lines_list, indexes_list=list(range(7)), labels=None, dt=1, xlabel=""
     liness = liness + lines2
     labelss = labelss + [ labels[i] for i in range(N)  ]
   fig.legend(liness, labelss, loc ="lower right",
-              mode=None, borderaxespad=1, ncol=3, fontsize=10)
+              mode=None, borderaxespad=1, ncol=4) # , fontsize=10
 
   fig.text(0.5, 0.14, xlabel, ha='center')
   fig.text(0.0, 0.5, ylabel, va='center', rotation='vertical')
-  fig.tight_layout(rect=[0., 0.16, 1, 0.95])
-
+  fig.tight_layout()
+  return axs.flat
 
 def save(filename, **kwargs):
   with open(filename, 'w') as f:
@@ -99,65 +99,81 @@ def plot_info(dt, learnable_joints=list(range(7)),
           v=True, p=True, dp=False, e_xyz=False, e=False, torque=False, N=None, M=1,
           fname=None, kinematics=None):
 
-  def save(typ):
+  def save(typ, axs):
     if fname is not None:
       fig = plt.gcf()
       # fig.set_size_inches((25.5, 8), forward=False)
       plt.savefig(fname+ "_" +typ + '.pdf', bbox_inches='tight')
+      
+      # for i, ax2 in enumerate(axs):
+      #   extent = full_extent(ax2).transformed(fig.dpi_scale_trans.inverted())
+      #   fname2 = fname+"/joint_{}/".format(learnable_joints[i])
+      #   if not os.path.exists(fname2):
+      #       os.makedirs(fname2)
+        
+      #   fig.savefig(fname2 + typ + "_" + '.pdf', bbox_inches=extent)
 
   qlim = kinematics.limits if kinematics is not None else None
   qvlim = kinematics.vlimits if kinematics is not None else None
 
   if joints_q_vec is not None and q_traj_des is not None and p:
-    line_list = [q_traj_des] + list(joints_q_vec[:N:M])
-    label_list = ["des"] + ["it="+str(i*M) for i in np.arange(len(line_list)-1)]
-    plot_A(line_list, learnable_joints, label_list, dt=dt, xlabel=r"$t$ [s]", ylabel=r"angle [$degree$]", limits=qlim)
+    N_ = N if N is not None else len(joints_q_vec)
+    its = [0] + [N_-i for i in np.arange(3,0,-1)]
+    line_list = [q_traj_des] + list(joints_q_vec[its])
+    label_list = ["des"] + ["it="+str(it) for it in its]
+    axs = plot_A(line_list, learnable_joints, label_list, dt=dt, xlabel=r"$t$ [s]", ylabel=r"angle [$degree$]", limits=qlim)
     plt.suptitle("Angle Positions")
-    save("angle_pos")
+    save("angle_pos", axs)
 
   if u_ff_vec is not None and q_v_traj is not None and v:
-    line_list = [q_v_traj] + list(u_ff_vec[:N:M])
-    label_list = ["performed"] + ["it="+str(i*M) for i in np.arange(len(line_list)-1)]
-    plot_A(line_list, learnable_joints, fill_between=[np.max(u_ff_vec, axis=0), np.min(u_ff_vec, axis=0)],
+    N_ = N if N is not None else len(u_ff_vec)
+
+    its = [0] + [N_-i for i in np.arange(3,0,-1)]
+    line_list = [q_v_traj] + list(u_ff_vec[its])
+    label_list = ["performed"] + ["it="+str(it) for it in its]
+    axs = plot_A(line_list, learnable_joints, fill_between=[np.max(u_ff_vec, axis=0), np.min(u_ff_vec, axis=0)],
            index_labels=[r"$\dot{\theta}_%d$" %(i+1) for i in learnable_joints], labels=label_list, limits=qvlim,
            dt=dt, xlabel=r"$t$ [s]", ylabel=r" angle velocity [$\frac{degree}{s}$]")
     plt.suptitle("Angle Velocities")
-    save("angle_vel")
+    save("angle_vel", axs)
 
   if disturbanc_vec is not None and dp:
-    line_list = list(disturbanc_vec[:N:M])
-    label_list = ["it="+str(i*M) for i in range(len(line_list))]
+    N_ = N if N is not None else len(disturbanc_vec)
+
+    its = [0] + [N_-i for i in np.arange(3,0,-1)]
+    line_list = list(disturbanc_vec[its])
+    label_list = ["it="+str(it) for it in its]
     index_labels=[r"$d_%d$" %(i+1) for i in learnable_joints]
-    plot_A(line_list, learnable_joints, label_list, dt=dt, xlabel=r"$t$ [s]", ylabel=r"angle [$degee$]",
+    axs = plot_A(line_list, learnable_joints, label_list, dt=dt, xlabel=r"$t$ [s]", ylabel=r"angle [$degee$]",
            index_labels=index_labels)
     plt.suptitle("Disturbance")
-    save("disturbance")
+    save("disturbance", axs)
 
   if d_xyz_rpy_vec is not None and e_xyz:
     ls = ['x', 'y', 'z', 'roll', 'pitch' ,'yaw']
-    plot_A(d_xyz_rpy_vec[0:2], list(range(6)), ["it0", "it1"], dt=dt, xlabel=r"$t$ [s]", ylabel=r"d [$m$]",
+    axs = plot_A(d_xyz_rpy_vec[0:2], list(range(6)), ["it0", "it1"], dt=dt, xlabel=r"$t$ [s]", ylabel=r"d [$m$]",
            index_labels=ls, degree=False, rows=1)
     plt.suptitle("Cartesian Error Trajectories")
-    save("d_xyz_rpy_vec")
+    save("d_xyz_rpy_vec", axs)
 
   if joint_error_norms is not None and e:
     index_labels=[r"$||\theta_%d - \theta^{des}_%d||_2$" %(i+1, i+1) for i in learnable_joints]
-    plot_A([joint_error_norms], learnable_joints, ["L2-norm"], xlabel=r"$IT$", ylabel=r"angle [$degree$]",
+    axs = plot_A([joint_error_norms], learnable_joints, ["L2-norm"], xlabel=r"$IT$", ylabel=r"angle [$degree$]",
            index_labels=index_labels)
     plt.suptitle("Joint angle errors through iterations")
-    save("joint_error")
+    save("joint_error", axs)
 
   if cartesian_error_norms is not None and e_xyz:
     labels = ["x", "y", "z", "nx", "ny", "nz"]
-    plot_A([cartesian_error_norms], list(range(6)), None, xlabel=r"$IT$", ylabel="L2 norm", index_labels=labels)
+    axs = plot_A([cartesian_error_norms], list(range(6)), None, xlabel=r"$IT$", ylabel="L2 norm", index_labels=labels)
     plt.suptitle("Cartesian errors through iterations")
-    save("cartesian_error")
+    save("cartesian_error", axs)
 
   if joint_torque_vec is not None and torque:
-    plot_A([joint_torque_vec[-1]], learnable_joints, ["torque"], dt=dt, xlabel=r"$t$", ylabel=r"Newton",
+    axs = plot_A([joint_torque_vec[-1]], learnable_joints, ["torque"], dt=dt, xlabel=r"$t$", ylabel=r"Newton",
            index_labels=[r"$M_%d$" %(i+1) for i in learnable_joints])
     plt.suptitle("Torque trajectories for each joint")
-    save("joint_torque")
+    save("joint_torque", axs)
 
   if fname is None:
     plt.show()
