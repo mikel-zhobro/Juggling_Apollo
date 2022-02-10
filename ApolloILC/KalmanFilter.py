a66fb2573950f7a72dfe65c0e20a2cd16df7643a
Mikel Zhobro <zhobromikel@gmail.com>
2022-02-10 11:54:18 +0100

    document kalman filter

--- ApolloILC/KalmanFilter.py
+++ ApolloILC/KalmanFilter.py
@@ -3,62 +3,69 @@ import numpy as np
 
 class KalmanFilter:
   def __init__(self, lss, M, d0, P0, epsilon0, freqDomain=False, epsilon_decrease_rate=0.9):
+    ## Prediction model
+    # d_{j+1} = d_{j} + n_d_{j} with d_{0}~N(0,P0) and n_d_{j}~N(0,Dj) where Dj = epsilon_{j}*eye(N)
+    ## Measurment model
+    # y_{j} = Fu_{j} + Gd0  + ((( GKd_{j} ))) + n_y  with n_y ~ N(0, M)
 
-    self.timeDomain = not freqDomain
-    # size of the state
+    # Size of the state
     self.N = d0.size
 
-    # pointer to the lifted space (can be changed from outside)
+    # In case the KF works in freq-domain its components should have an imaginary part, thats what we prepare here.
+    self.timeDomain = not freqDomain
+    imag = 1j if freqDomain else 0.0
+    self.Ident = (1+imag)*np.eye(self.N)
+
+    # Pointer to the lifted space (can be changed from outside)
     self.lss = lss
     self.epsilon_decrease_rate = epsilon_decrease_rate
 
-    imag = 1j if freqDomain else 0.0
-    self.Ident = (1+imag)*np.eye(self.N)
-    # initial values
-    # di = di_0 + ni with ni~N(0,Omega) <- Omega = epsilon*eye(N)
-    self.M = np.diag((1+imag)*M)            # covariance of noise on the measurment
-    # self.M = self.lss.GK.dot(np.diag((1+imag)*M)).dot(self.lss.GK.T.conj())
+    # Covariance of the noise on the measurment
+    self.M = np.diag((1+imag)*M)
+    # self.M = self.lss.GK.dot(np.diag((1+imag)*M)).dot(self.lss.GK.T.conj())  # to use if we want to initialize the measurement-noise covariance in disturbance level.
+
+    # Initial values
     self.d0 = d0+0*imag                     # initial disturbance value
     self.P0 = np.diag((1+imag)*P0)          # initial disturbance covariance
     self.D0 = epsilon0*self.Ident           # covariance of noise on the disturbance
 
-    # current values
-    self._d = None
-    self._P = None
-    self._D = None
+    # Current values
+    self._dj = None
+    self._Pj = None
+    self._Dj = None
 
   @property
   def d(self):
-    if self._d is None:
+    if self._dj is None:
       raise ValueError("The Kalman Filter is not yet reseted(d is not initialized))")
     else:
-      return self._d
+      return self._dj
 
   @property
   def P(self):
-    if self._P is None:
+    if self._Pj is None:
       raise ValueError("The Kalman Filter is not yet reseted(P is not initialized))")
     else:
-      return self._P
+      return self._Pj
 
   def resetKF(self, d=None, P=None):
-      self._d = self.d0 if d is None else d
-      # self._P = self.P0
+      self._dj = self.d0 if d is None else d
+      # self._Pj = self.P0
       if P is None:
         P = self.P0
-      self._P = self.lss.GK.dot(P).dot(self.lss.GK.T.conj()) if False and self.timeDomain else P               # TODO: Is this needed?
-      self._D = self.lss.GK.dot(self.D0).dot(self.lss.GK.T.conj()) if False and self.timeDomain else self.D0   # TODO: Is this needed?
+      self._Pj = self.lss.GK.dot(P).dot(self.lss.GK.T.conj()) if False and self.timeDomain else P               # TODO: Is this needed?
+      self._Dj = self.lss.GK.dot(self.D0).dot(self.lss.GK.T.conj()) if False and self.timeDomain else self.D0   # TODO: Is this needed?
 
   def updateStep(self, u, y_meas):
     # In this case
     # d0 ~ N(P0)
     # d = d + n_d                        with n_d ~ N(eps*I)
     # y = Fu + Gd0  + ((( GKd ))) + n_y  with n_y ~ N(0, M)
-    P1_0 = self._P + self._D
+    P1_0 = self._Pj + self._Dj
     Theta = self.lss.GK.dot(P1_0).dot(self.lss.GK.T.conj()) + self.M
     K = P1_0.dot(self.lss.GK.T.conj()).dot(np.linalg.pinv(Theta))
 
-    # Weight
+    # Weight (can be used to scale the Kalman gain. Here we force the KF to make very little changesif to make KF less agressive in the beginning of the disturbance trajectory)
     if False and self.timeDomain:
       N = y_meas.size
       # D = np.diag(np.linspace(0,1.0,N))
@@ -68,10 +75,10 @@ class KalmanFilter:
       # plt.show()
       K = D.dot(K)
 
-
-    self._P = (self.Ident - K.dot(self.lss.GK)).dot(P1_0)
-    self._d = self._d + K.dot(y_meas - self.lss.Gd0 - self.lss.GK.dot(self._d) - self.lss.GF.dot(u))
+    # update estimated state and its covariance
+    self._dj = self._dj + K.dot(y_meas - self.lss.Gd0 - self.lss.GK.dot(self._dj) - self.lss.GF.dot(u))
+    self._Pj = (self.Ident - K.dot(self.lss.GK)).dot(P1_0)
 
     # update epsilon
-    self._D *= self.epsilon_decrease_rate
-    return self._d
+    self._Dj *= self.epsilon_decrease_rate
+    return self._dj
