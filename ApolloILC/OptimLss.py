8393189acc34e99ce8870bb095c1668a5a2fe2c0
Mikel Zhobro <zhobromikel@gmail.com>
2022-02-16 15:05:30 +0100

    OptimLSS: doc

--- ApolloILC/OptimLss.py
+++ ApolloILC/OptimLss.py
@@ -7,7 +7,7 @@
 @Version :   1.0
 @Contact :   zhobromikel@gmail.com
 @License :   (C)Copyright 2021-2022, Mikel Zhobro
-@Desc    :   the optimization problem to compute feedforward input for the next iteration
+@Desc    :   The optimization problem to compute feedforward input for the next iteration.
 '''
 
 import numpy as np
@@ -19,12 +19,14 @@ class OptimLss:
     self.lss = lifted_state_space
 
   def calcDesiredInput(self, d, delta_y_des, print_norm=False, lb=None, ub=None):
-    # TODO: for delta way, no need for delta_ydes
-    # Solves  GFdelta_u = delta_ydes -(GKd + Gd0)
-    # as optimization
-    # delta_u = argmin_u |Weight[delta_ydes - (GKd + Gd0) - GFdelta_u]|_2 + sigma*|delta_u|_2 + mu*|(I-I_1) delta_u|_2
+    # The LSS dictates what we want to optimize on:
+    #       delta_y = GF delta_u + GK d + G d0 + GF_feedback ydes
+    # min_{delta_u} ||GF delta_u + GK d + G d0 + GF_feedback ydes||_2
+    # after adding a scaling matrix(weigt), penalty input's amplitude(sigma), and loss for fast changes of input(mu)
+    # we can write the minimization problem in its general form:
+    #       delta_u_des = argmin_u ||Weight[(GKd + Gd0 - delta_y_des) + GFdelta_u]||_2 + sigma*||delta_u||_2 + mu*||(I-I_1) delta_u||_2
     # Which corresponds in setting the first gradient to 0, i.e. solving
-    # (GF^T*W*GF + sigma*I + mu*(I-I_1)) * delta_u = GF^T* W * (delta_ydes - (GKd + Gd0))  <=> Au = b
+    #       (GF^T*W*GF + sigma*I + mu*(I-I_1)) * delta_u = GF^T* W * (delta_y_des - GKd - Gd0)  <=> Au = b
     N_y = delta_y_des.shape[0]
     N_u = self.lss.GF.shape[1]
 
@@ -34,12 +36,14 @@ class OptimLss:
 
     if lb is None and ub is None:
       P = (self.lss.GF.T).dot(Weight).dot(self.lss.GF)   # close to the desired output N_u x N_u
-      Q = 0.00001*np.eye(N_u)                             # possibly small inputs  N_u x N_u
-      S = 0.0000001*(np.eye(N_u)-np.eye(N_u, k=1))        # slow changes  N_u x N_u
+      Q = 0.00001*np.eye(N_u)                            # possibly small inputs  N_u x N_u
+      S = 0.0000001*(np.eye(N_u)-np.eye(N_u, k=1))       # slow changes  N_u x N_u
       A = P + Q + S
       b = self.lss.GF.T.dot(Weight).dot(delta_y_des - self.lss.GK.dot(d) - self.lss.Gd0)
       delta_u_des = np.linalg.inv(A).dot(b)
     else:
+      # here we solve the least squared error optimization problem with inequality constrained
+      # we use the unconstrained solution as initial solution
       from scipy.optimize import minimize
       def loss(x):
         y = self.lss.GF.dot(x.reshape(-1, 1)) + self.lss.Gd0  + self.lss.GK.dot(d) # predicted y
@@ -55,7 +59,7 @@ class OptimLss:
       x0 = np.clip(self.calcDesiredInput(d, delta_y_des), lb, ub)
 
       delta_u_des = minimize(loss, x0, method='trust-krylov', jac=jacob, hess=hessian,
-                    bounds=[(lb, ub) for i in range(N)]).x.reshape(-1,1)
+                    bounds=[(lb, ub) for i in range(N_u)]).x.reshape(-1,1)
 
       print("Change after constraints", np.linalg.norm(x0 - delta_u_des))
 
